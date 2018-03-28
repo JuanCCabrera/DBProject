fdac28e3d338d1472baf80fcfa90b4bcbafb18f6
coraliscamacho1 <coralis.camacho1@upr.edu>
2018-03-27 21:42:28 -0400

    Messages in a Group Chat by User ID Join

--- dao/Message.py
+++ dao/Message.py
@@ -4,11 +4,15 @@ class MessageDAO:
         P2 = [59, 'estoy escribiendo de nuevo','01/01/2018',False, 97, 100]
         P3 = [57, 'No explota como Siquitrake','01/01/2018',False, 563, 101]
         P4 = [58, 'Jaaaajajajaja #tru #hard','01/02/2018',True, 787, 102]
+        P5 = [60, 'esto es una prueba','01/01/2018',False, 97, 101]
+        P6 = [61, 'estoy probando tambien', '01/01/2018', False, 97, 102]
         self.data = []
         self.data.append(P1)
         self.data.append(P2)
         self.data.append(P3)
         self.data.append(P4)
+        self.data.append(P5)
+        self.data.append(P6)
 
     def getAllMessages(self):
         return self.data
@@ -28,6 +32,17 @@ class MessageDAO:
         for r in self.data:
             if cid == r[4] :
                 result.append(r)
+        if not result:
+            return None
+        else:
+            return result
+
+    def getMessagesbyChatIDAndUser(self,cid, uid):
+        result = []
+        for r in self.data:
+            if cid == r[4] :
+                if uid == r[5]:
+                    result.append(r)
         if not result:
             return None
         else:
